7c39b44f2af633f4549b09012e6bed59dc7a81e1
FelipeJhordan <felipejordan.alves@gmail.com>
2022-09-27 23:12:52 -0300

    feat: finish the post card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,23 +2,15 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { PostWidget, PostCard, Categories } from "../components/";
 import { Post } from "../protocols/models";
+import { Props } from "../protocols/react/Props";
+import { getPosts } from "../services/graphl/graphlService";
+import { NodePost } from "../services/graphl/protocols/nodePost";
 
-const post: Post[] = [
-  {
-    title: "React testing",
-    excerpt: "Learn React Testing",
-  },
-  {
-    title: "React with Tailwind",
-    excerpt: "Learn React with Tailwind",
-  },
-  {
-    title: "Learn React with Typescript",
-    excerpt: "Learn React with Typescript Testing",
-  },
-];
+type HomeProps = Props & {
+  posts: Post[];
+};
 
-const Home: NextPage = () => {
+const Home: NextPage<HomeProps> = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -27,8 +19,8 @@ const Home: NextPage = () => {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {post.map((post) => (
-            <PostCard post={post} key={post.title}></PostCard>
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title + post.excerpt} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -40,6 +32,19 @@ const Home: NextPage = () => {
       </div>
     </div>
   );
+};
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  console.log(posts);
+  const serializedPosts = serializePosts(posts);
+  return {
+    props: { posts: serializedPosts },
+  };
+}
+
+export const serializePosts = (posts: Record<string, {}>[]) => {
+  return posts.map((post: Record<string, {}>) => post.node);
 };
 
 export default Home;
