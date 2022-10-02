076b304e902eb77594db60e07a8797ec4365463c
FelipeJhordan <felipejordan.alves@gmail.com>
2022-10-01 22:44:58 -0300

    feat: create a service to graphl to find relatedPosts and similarPosts

--- services/graphl/graphlService.ts
+++ services/graphl/graphlService.ts
@@ -1,4 +1,6 @@
 import request, { gql } from "graphql-request"
+import { FeaturedImage } from "../../protocols/models"
+import { RelatedPosts } from "../../protocols/models/RelatedPosts"
 
 const graphqlApi = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT ?? ''
 
@@ -37,3 +39,50 @@ export const getPosts = async () => {
     const result = await request(graphqlApi, query)
     return result.postsConnection.edges
 } 
+
+export const getRecentPosts = async (): Promise<RelatedPosts[]> => {
+  const query = gql`
+  query getpostsdetail {
+    posts(orderBy: createdAt_ASC, last: 3) {
+      slug
+      title
+      createdAt
+      featuredImage {
+        url
+      }
+    }
+  }
+  `;
+  const result = await request(graphqlApi, query);
+  console.log(result)
+
+  return result.posts;
+};
+
+export const getSimilarPosts = async (categories: [], slug: string) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug, AND: {categories_some: {
+            slug_in: $categories
+          }}
+        },
+        last: 3
+      ) {
+        {
+          title
+          featuredImage {
+            url
+          }
+          createdAt
+          slug
+        }
+      }
+    }
+  `
+
+  const result = await request(graphqlApi, query)
+
+  return result.postsConnection.posts
+}
